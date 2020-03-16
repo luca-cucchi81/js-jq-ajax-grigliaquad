e0ac676d2f84c83bccbeddb1038f2215d63bdafe
Luca <lu.cucchi@gmail.com>
2020-03-16 15:23:59 +0100

    fix note a margine del js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 // SCOPO DEL GIOCO: Griglia 6x6, ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9. Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde. Il numero ottenuto appare al centro del quadrato.
+
+// avvio un ciclo for che mi crea 36 quadrati basati sul template di handlebars
 for (var i = 0; i < 36; i++) {
     var source = $('#square-template').html();
     var template = Handlebars.compile(source);
 
-    var context = { numero: ""};
+    var context = { numero: ""};  //il contenuto della chiave è vuoto e andrà riempito con la chiamata ajax
     var num = template(context);
     $('.container').append(num);
 }
 
+// al click su ognuno dei quadrati creati avvio una chimata ajax che, in caso di successo, mi cambia il contenuto della chiave handlebars e il colore di sfondo a seconda del risultato
 $(document).on('click', '.square', function(event){
     if ($(event.target).find('h1').text() == ""){
         $.ajax({
